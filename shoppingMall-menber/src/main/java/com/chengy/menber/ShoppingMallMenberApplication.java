d1ada5c9f4a88bbd6fffd87d7190eb726a918360
chengy <1061332369@qq.com>
2022-08-11 23:13:33 +0800

    提交代码

---
+++ shoppingMall-menber/src/main/java/com/chengy/menber/ShoppingMallMenberApplication.java
@@ -0,0 +1,13 @@
+package com.chengy.menber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallMenberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallMenberApplication.class, args);
+    }
+
+}
---
+++ shoppingMall-order/src/test/java/com/chengy/order/shoppingmallorder/ShoppingMallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengy.order.shoppingmallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
