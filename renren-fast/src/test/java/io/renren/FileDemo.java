205a09593690a2858cea682059d1c540e873812d
chengy <1061332369@qq.com>
2022-08-24 15:20:21 +0800

    gateway解决503，正确获取到验证码

---
+++ renren-fast/src/test/java/io/renren/FileDemo.java
@@ -0,0 +1,24 @@
+package io.renren;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.sql.Blob;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileDemo {
+
+
+//    String filePath = "/Users/chengy/IdeaProjects/GuiGuShoppingMall/renren-fast/src/main/resources/static/swagger/images/expand.gif";
+    @Test
+    public void  testFile() throws FileNotFoundException {
+
+
+    }
+}
